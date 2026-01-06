adf19b2dfd60867b174c33bc398c704594a36629
Kyle Harris <digibee@gmail.com>
2026-01-06 09:21:06 -0500

    app.py changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, random, yaml
+import os, random, yaml, datetime
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 
@@ -12,20 +12,22 @@ def is_image(i):
     return i.lower().endswith("jpg") or i.lower().endswith("jpeg")
 
 
+def write_forms(d):
+    forms = os.path.join(home, "forms")
+    with open(forms, "a") as f:
+        f.write(d)
+
+
 home = os.path.dirname(__file__)
 yaml_file = os.path.join(home, "config.yml")
 with open(yaml_file, "r") as f:
     config = yaml.load(f.read(), Loader=yaml.Loader)
-
-secrets = {"flask_key": "this is the flask secret key"}
-secrets_file = os.path.join(".secrets")
-if os.path.exists(secrets_file):
-    with open(secrets_file, "r") as f:
-        secrets = yaml.load(f.read(), Loader=yaml.Loader)
+flask_key = os.getenv("FLASK_KEY", "this is the flask key")
+check_password = os.getenv("CHECK_PASSWORD", "")
 
 
 app = Flask(__name__)
-app.secret_key = secrets["flask_key"]
+app.secret_key = flask_key
 
 sections = []
 gallery_images = []
@@ -54,7 +56,10 @@ for section in config["sections"]:
                 images.append({"path": image_path, "desc": desc})
                 ign = image_path.split(".")[0] + ".ign"
                 if not os.path.exists(ign):
-                    gallery_images.append({"path": image_path, "desc": ""})
+                    desc = ""
+                    if debug:
+                        desc = fname
+                    gallery_images.append({"path": image_path, "desc": desc})
         topic.setdefault("tag", topic["path"].split(os.sep)[-1])
         topic.setdefault("images", images)
         log(topic)
@@ -64,7 +69,10 @@ for path in gallery["paths"]:
         log(fname)
         if is_image(fname):
             image_path = os.path.join(path, fname)
-            gallery_images.append({"path": image_path, "desc": ""})
+            desc = ""
+            if debug:
+                desc = fname
+            gallery_images.append({"path": image_path, "desc": desc})
 random.shuffle(gallery_images)
 gallery.setdefault("topics", [{"name": "Gallery", "tag": "gallery", "images": gallery_images}])
 
@@ -72,7 +80,7 @@ log(config)
 
 @app.route("/")
 def index():
-    return render_template('index.html', config=config)
+    return render_template("index.html", config=config)
 
 @app.route("/contact", methods=["POST"])
 def contact():
@@ -80,12 +88,23 @@ def contact():
     email = request.form.get("email")
     phone = request.form.get("phone")
     message = request.form.get("message")
-
-    with open("forms", "a") as f:
-        f.write(f"{name}\n{email}\n{phone}\n{message}\n----\n")
-
+    ts = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%dT%H:%M:%S')
+    write_forms(f"\n----\n{ts}\n{name}\n{email}\n{phone}\n{message}\n----\n")
     return {"status": "success"}
 
+@app.route("/check_messages")
+def check_messages():
+    password = request.args.get("password")
+
+    ts = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%dT%H:%M:%S')
+    write_forms(f"\n++++\n{ts}\n{password}\n\+++++\n")
+    if check_password and (password == check_password):
+        forms = os.path.join(home, "forms")
+        with open(forms, "r") as f:
+            data = f.read().splitlines()
+        return render_template("check_messages.html", data=data)
+    else:
+        return "Access denied", 403
 
 if __name__ == '__main__':
     app.run(debug=True)
